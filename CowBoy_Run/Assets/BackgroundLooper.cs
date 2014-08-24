a895fe78e3a79d61a5e0c3b9c8cbb62515cdb066
labonipaul <laboniku110224@gmail.com>
2014-08-24 10:17:50 +0600

    Background Looper 2

---
+++ CowBoy_Run/Assets/BackgroundLooper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundLooper : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		Debug.Log ("Trigger :"+ collider.name);
+	}
+}
