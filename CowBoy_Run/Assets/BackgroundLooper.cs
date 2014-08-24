e97a8b2274f08688ec5652d4996de1f483fa254d
labonipaul <laboniku110224@gmail.com>
2014-08-24 10:23:54 +0600

    Background Looper 4

--- CowBoy_Run/Assets/BackgroundLooper.cs
+++ CowBoy_Run/Assets/BackgroundLooper.cs
@@ -6,10 +6,12 @@ public class BackgroundLooper : MonoBehaviour {
 	int numBackgrounPanels = 4;
 	void OnTriggerEnter2D(Collider2D collider)
 	{
-		Debug.Log ("Triggered "+ collider.name);
+		//Debug.Log ("Triggered "+ collider.name);
 		float widthOfBackgroundObject = ((BoxCollider2D)collider).size.x;
 		Vector3 pos = collider.transform.position;
-		pos.x += widthOfBackgroundObject * numBackgrounPanels;
+	//	pos.x += widthOfBackgroundObject * numBackgrounPanels;
+		pos.x += widthOfBackgroundObject * numBackgrounPanels - widthOfBackgroundObject/2 ;
+
 		collider.transform.position = pos;
 	}
 }
