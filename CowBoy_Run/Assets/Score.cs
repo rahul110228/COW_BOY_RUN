8bc2495e4b35d54dc0c4a463e483b5fb917b91bd
rahul110228 <cserahul.ku@gmail.com>
2014-08-24 10:52:23 +0600

    Score

---
+++ CowBoy_Run/Assets/Score.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public int startTime=0;
+	public int stageTime=0;
+	//GUIText guiText;
+	void Start () {
+		startTime = (int)Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//GetComponent (GUIText).text = Time.time.ToString; 
+		stageTime = (int)Time.time - startTime;
+
+	}
+	void OnGUI() {
+		//GUI.Label(new Rect(10, 10, 150, 150), stageTime.ToString());
+		//GUIText.te (stageTime.ToString());
+		guiText.text = stageTime.ToString()+ " m";        
+	}
+}
