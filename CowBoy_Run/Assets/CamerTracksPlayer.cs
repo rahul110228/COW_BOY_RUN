5eb7d2e107effe68b107dcb3518a54530ff20661
labonipaul <laboniku110224@gmail.com>
2014-08-24 10:12:32 +0600

    Camera

---
+++ CowBoy_Run/Assets/CamerTracksPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamerTracksPlayer : MonoBehaviour {
+	Transform player;
+	float offsetx;
+	// Use this for initialization
+	void Start () {
+		GameObject player_move = GameObject.FindGameObjectWithTag ("Player");
+		player = player_move.transform;
+		offsetx = transform.position.x - player.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
