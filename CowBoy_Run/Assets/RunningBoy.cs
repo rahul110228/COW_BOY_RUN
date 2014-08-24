07f1cb03294dda125d476f9871257bddb1a0bdae
apusaha <cseapu.ku@gmail.com>
2014-08-24 10:00:10 +0600

    Running2

---
+++ CowBoy_Run/Assets/RunningBoy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunningBoy : MonoBehaviour {
+
+	Vector3 velocity = Vector3.zero;
+	public Vector3 gravity;
+	public Vector3 flapVelocity;
+	public float maxSpeed = 5f;
+	public float forwardSpeed = 1f;
+	
+	bool dipFlap = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Do graphics and input update 
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+			dipFlap = true;		
+		}
+	}
+	
+	//Do physics engines update here
+	void FixedUpdate(){ 
+		velocity.x = forwardSpeed;
+		velocity += gravity * Time.deltaTime;
+		
+		if (dipFlap == true) {
+			dipFlap = true;
+			velocity += flapVelocity;
+		}
+		velocity = Vector3.ClampMagnitude (velocity, maxSpeed);
+		transform.position += velocity * Time.deltaTime;
+	}
+}
