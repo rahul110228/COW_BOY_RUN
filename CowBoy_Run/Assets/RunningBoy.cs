194920f7aafda73c501ddcf7f8db5f87c6f827c3
rahul110228 <cserahul.ku@gmail.com>
2014-08-24 10:30:33 +0600

    GroundProtect

--- CowBoy_Run/Assets/RunningBoy.cs
+++ CowBoy_Run/Assets/RunningBoy.cs
@@ -4,12 +4,11 @@ using System.Collections;
 public class RunningBoy : MonoBehaviour {
 
 	Vector3 velocity = Vector3.zero;
-	public Vector3 gravity;
 	public Vector3 flapVelocity;
-	public float jumpSpeed = 5f;
+	public float maxSpeed = 5f;
 	public float forwardSpeed = 1f;
 	
-	bool manJump = false;
+	bool dipFlap = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -18,37 +17,20 @@ public class RunningBoy : MonoBehaviour {
 	// Do graphics and input update 
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
-			manJump = true;		
+			dipFlap = true;		
 		}
 	}
 	
 	//Do physics engines update here
 	void FixedUpdate(){ 
 		velocity.x = forwardSpeed;
-		velocity += gravity * Time.deltaTime;
+		//velocity += gravity * Time.deltaTime;
 		
-		if (manJump == true) {
-			manJump = true;
-			velocity += flapVelocity;
-			if(velocity.y <0)
-			{
-				velocity.y = 0;
-			}
+		if (dipFlap == true) {
+			dipFlap = true;
 			velocity += flapVelocity;
 		}
-		velocity = Vector3.ClampMagnitude (velocity, jumpSpeed);
+		velocity = Vector3.ClampMagnitude (velocity, maxSpeed);
 		transform.position += velocity * Time.deltaTime;
-
-		if (manJump) {
-			rigidbody2D.AddForce(Vector2.up* jumpSpeed);
-			manJump = false;
-			
-		}
-
-		float angle = 0;
-		if (velocity.y < 0) {
-			angle = Mathf.Lerp(0, -90, -velocity.y /jumpSpeed);
-		}
-		transform.rotation = Quaternion.Euler (0, 0, angle);
 	}
 }
