567e8a3f6b0c4e3053004cef8cc73e0dc611a85b
rahul110228 <cserahul.ku@gmail.com>
2014-08-24 10:33:09 +0600

    GroundProyect 2

--- CowBoy_Run/Assets/RunningBoy.cs
+++ CowBoy_Run/Assets/RunningBoy.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class RunningBoy : MonoBehaviour {
 
 	Vector3 velocity = Vector3.zero;
-	public Vector3 flapVelocity;
-	public float maxSpeed = 5f;
+	//public Vector3 flapVelocity;
+	//public float maxSpeed = 5f;
 	public float forwardSpeed = 1f;
+	public float jumpSpeed = 9.8f;
+
 	
 	bool dipFlap = false;
 	// Use this for initialization
@@ -19,18 +21,12 @@ public class RunningBoy : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
 			dipFlap = true;		
 		}
-	}
+	} 
 	
 	//Do physics engines update here
 	void FixedUpdate(){ 
-		velocity.x = forwardSpeed;
-		//velocity += gravity * Time.deltaTime;
-		
-		if (dipFlap == true) {
-			dipFlap = true;
-			velocity += flapVelocity;
-		}
-		velocity = Vector3.ClampMagnitude (velocity, maxSpeed);
-		transform.position += velocity * Time.deltaTime;
-	}
+		rigidbody2D.AddForce (Vector2.right * forwardSpeed);
+		rigidbody2D.AddForce (Vector2.up * jumpSpeed);
+
+	} 
 }
